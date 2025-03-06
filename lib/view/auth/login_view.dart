efc724554a8c8b48d33db75d445e0961f5e6928a
jannatulferdous2730 <jannatulferdous.2730@gmail.com>
2025-03-06 23:50:43 +0600

    Enhance login view layout; add logo and form fields for email and password

--- lib/view/auth/login_view.dart
+++ lib/view/auth/login_view.dart
@@ -1,15 +1,71 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:triangle/utils/constants/app_colors.dart';
+
+import '../../utils/constants/sizes.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-    return const Scaffold(
-      body: Center(
-        child: Text("Welcome"),
-      )
-    );
+    return Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(Sizes.lg),
+          child: SingleChildScrollView(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Column(
+                    children: [
+                      Image(image: AssetImage("assets/images/app_logo.png")),
+                    ],
+                  ),
+                  const SizedBox(height: Sizes.spaceBtwSections),
+                  Form(
+                    child: Column(children: [
+                      TextFormField(
+                        decoration: const InputDecoration(hintText: "Email"),
+                      ),
+                      const SizedBox(height: Sizes.spaceBtwInputFields),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "Password",
+                          suffixIcon: Icon(CupertinoIcons.eye_slash),
+                        ),
+                      ),
+                      // const SizedBox(height: Sizes.spaceBtwItems),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                            Checkbox(value: true, onChanged: (value) {}),
+                            const Text("Remember me")
+                          ]),
+                      
+                          TextButton(onPressed: () {}, child: const Text("Forgot Password?", style: TextStyle(color: AppColor.secondary, fontWeight: FontWeight.w600),))
+                        ],
+                      ),
+                      
+                      const SizedBox(height: Sizes.spaceBtwSections),
+                      
+                      FilledButton(onPressed: () {}, child: const Text("Login")),
+                      
+                      const SizedBox(height: Sizes.spaceBtwSections),
+                      
+                      const Text("Don't have an account?", style: TextStyle(color: AppColor.textSecondary),),
+                      const SizedBox(height: Sizes.spaceBtwItems),
+                      OutlinedButton(onPressed: () {}, child: const Text("Register"))
+                    ]),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
