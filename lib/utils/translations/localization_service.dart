d5d7874cd4b09f1a45ad9287b67de79d5f790315
jannatulferdous2730 <jannatulferdous.2730@gmail.com>
2025-03-23 02:26:31 +0600

    Add initial localization setup

---
+++ lib/utils/translations/localization_service.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+
+class LocalizationService extends Translations {
+  @override
+  Map<String, Map<String, String>> get keys => {
+        'en_US': {"termsAndConditions": "I agree to the terms and conditions"},
+        "bn_BD": {"termsAndConditions": "আমি শর্তাবলী সম্পর্কে সম্মত হয়েছি"},
+      };
+}
