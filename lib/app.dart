7b01fbbf74faa83458a1c1f1324b7415d0a84c0c
minhazul73 <minhazul73@gmail.com>
2025-04-14 16:23:21 +0600

    feat: Integrate Firebase, enhance authentication, and update UI components

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:triangle/utils/constants/app_colors.dart';
 import 'package:triangle/view/auth/login_view.dart';
 
+import 'bindings/general_bindings.dart';
 import 'utils/theme/theme.dart';
 import 'utils/translations/localization_service.dart';
 import 'view/auth/register_view.dart';
@@ -17,10 +19,12 @@ class LittleSteps extends StatelessWidget {
       translations: LocalizationService(),
       locale: Get.deviceLocale,
       fallbackLocale: const Locale('en', 'US'),
+      initialBinding: GeneralBindings(),
       themeMode: ThemeMode.system,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      home: const RegisterView(),
+      // home: const RegisterView(),
+      home: const Scaffold(body: Center(child: CircularProgressIndicator(),),),
     );
   }
 }
