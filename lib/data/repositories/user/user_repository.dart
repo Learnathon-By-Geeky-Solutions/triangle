cf7f27637f740cd06beb88c573a3bd1d727df682
minhazul73 <minhazul73@gmail.com>
2025-04-14 16:20:32 +0600

    feat: Add Firebase integration and user authentication
    - Introduced firebase.json for Firebase configuration.
    - Implemented UserModel for user data representation.
    - Created AuthenticationRepository for handling user authentication.
    - Developed UserRepository for saving user data to Firestore.
    - Added network connectivity management with NetworkManager.
    - Implemented loaders and popups for user feedback during operations.
    - Created login and registration controllers for managing user sessions.
    - Added views for email verification and home screen.
    - Integrated splash screen configuration with splash.yaml.
    - Added utility classes for exception handling and data formatting.

---
+++ lib/data/repositories/user/user_repository.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:triangle/data/models/user/user_model.dart';
+import 'package:triangle/utils/exceptions/firebase_exceptions.dart';
+
+import '../../../utils/exceptions/format_exceptions.dart';
+import '../../../utils/exceptions/platform_exceptions.dart';
+
+class UserRepository extends GetxController {
+  static UserRepository get instance => Get.find();
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+
+  Future<void> saveUserData(UserModel user) async {
+    try {
+      await _firestore.collection("users").doc(user.id).set(user.toJson());
+    } on FirebaseException catch (e) {
+      throw AppFirebaseException(e.code).message;
+    } on FormatException catch (_) {
+      throw const AppFormatException();
+    } on PlatformException catch (e) {
+      throw AppPlatformException(e.code).message;
+    } catch (e) {
+      throw "An unexpected error occurred. Please try again.";
+    }
+  }
+}
---
+++ lib/view_model/auth/register_controller.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:triangle/data/models/user/user_model.dart';
+import 'package:triangle/data/repositories/user/user_repository.dart';
+import 'package:triangle/utils/helpers/network_manager.dart';
+import 'package:triangle/utils/popups/loaders.dart';
+import 'package:triangle/view/home/home_view.dart';
+
+import '../../data/repositories/auth/authentication_repository.dart';
+import '../../utils/popups/full_screen_loader.dart';
+
+class RegisterController extends GetxController {
+  static RegisterController get instance => Get.find();
+
+  final GlobalKey<FormState> registerFormKey = GlobalKey<FormState>();
+  final firstNameController = TextEditingController();
+  final lastNameController = TextEditingController();
+  final emailController = TextEditingController();
+  final phoneController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+  var passwordVisible = false.obs;
+  var termsAndConditions = false.obs;
+
+  void register() async {
+    try {
+      FullScreenLoader.openLoadingDialog("Processing your information", "assets/animations/animated_circular_loader.json");
+
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (!isConnected) {
+        
+        FullScreenLoader.stopLoading();
+        return;
+      }
+
+      if (!registerFormKey.currentState!.validate()) {
+        FullScreenLoader.stopLoading();
+        return;
+      }
+
+      if (!termsAndConditions.value) {
+        AppLoaders.warningSnackBar(title: "Terms and Conditions", message: "Please accept the terms and conditions");
+        FullScreenLoader.stopLoading();
+        return;
+      }
+
+      final userCredential = await AuthenticationRepository.instance.registerWithEmailAndPassword(
+        emailController.text.trim(),
+        passwordController.text.trim(),
+      );
+
+
+      final newUser = UserModel(
+        id: userCredential.user!.uid,
+        email: emailController.text.trim(),
+        firstName: firstNameController.text.trim(),
+        lastName: lastNameController.text.trim(),
+        phoneNumber: phoneController.text.trim(),
+        profilePicture: "",
+      );
+
+
+      final userRepository = Get.put(UserRepository());
+      await userRepository.saveUserData(newUser);
+
+      FullScreenLoader.stopLoading();
+
+      AppLoaders.successSnackBar(title: "Congratulations", message: "Account created successfully");
+
+      Get.offAll(() => const HomeView());
+
+    }
+    catch (e) {
+      FullScreenLoader.stopLoading();
+
+      AppLoaders.errorSnackBar(title: "Error", message: e.toString());
+    }
+  }
+}
