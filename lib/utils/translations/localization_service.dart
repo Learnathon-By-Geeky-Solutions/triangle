1a6eb60ab7cd80e1a6be3a481e12999c3fc0325a
minhazul73 <minhazul73@gmail.com>
2025-04-14 16:23:21 +0600

    feat: Integrate Firebase, enhance authentication, and update UI components

--- lib/utils/translations/localization_service.dart
+++ lib/utils/translations/localization_service.dart
@@ -4,7 +4,7 @@ import 'package:get/get.dart';
 class LocalizationService extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
-        'en_US': {"termsAndConditions": "I agree to the terms and conditions"},
-        "bn_BD": {"termsAndConditions": "আমি শর্তাবলী সম্পর্কে সম্মত হয়েছি"},
+        'en_US': {"termsAndConditions": " I agree to the terms and conditions"},
+        "bn_BD": {"termsAndConditions": " আমি শর্তাবলী সম্পর্কে সম্মত হয়েছি"},
       };
 }
--- lib/view/auth/register_view.dart
+++ lib/view/auth/register_view.dart
@@ -6,25 +6,21 @@ import 'package:triangle/utils/theme/extentions/custom_color_extention.dart';
 import '../../utils/constants/app_colors.dart';
 import '../../utils/constants/sizes.dart';
 import '../../utils/validator/app_validator.dart';
+import '../../view_model/auth/register_controller.dart';
 
 class RegisterView extends StatelessWidget {
   const RegisterView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-    final nameController = TextEditingController();
-    final emailController = TextEditingController();
-    final phoneController = TextEditingController();
-    final passwordController = TextEditingController();
-    final confirmPasswordController = TextEditingController();
-    var passwordVisible = false.obs;
+    
+    final controller = Get.put(RegisterController());
 
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(Sizes.lg),
         child: Form(
-          key: formKey,
+          key: controller.registerFormKey,
           child: SingleChildScrollView(
             child: Column(
               children: [
@@ -44,10 +40,26 @@ class RegisterView extends StatelessWidget {
                     const SizedBox(
                       height: Sizes.spaceBtwInputFields,
                     ),
-                    TextFormField(
-                      decoration: const InputDecoration(hintText: "Name"),
-                      controller: nameController,
-                      validator: (value) => AppValidator.validateName(value),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextFormField(
+                            expands: false,
+                            decoration: const InputDecoration(hintText: "First Name"),
+                            controller: controller.firstNameController,
+                            validator: (value) => AppValidator.validateName(value),
+                          ),
+                        ),
+                        const SizedBox(width: Sizes.spaceBtwInputFields),
+                        Expanded(
+                          child: TextFormField(
+                            expands: false,
+                            decoration: const InputDecoration(hintText: "Last Name"),
+                            controller: controller.lastNameController,
+                            validator: (value) => AppValidator.validateName(value),
+                          ),
+                        ),
+                      ],
                     ),
                     const SizedBox(
                       height: Sizes.spaceBtwInputFields,
@@ -55,7 +67,7 @@ class RegisterView extends StatelessWidget {
                     TextFormField(
                       decoration:
                           const InputDecoration(hintText: "Email address"),
-                      controller: emailController,
+                      controller: controller.emailController,
                       validator: (value) => AppValidator.validateEmail(value),
                     ),
                     const SizedBox(
@@ -64,7 +76,7 @@ class RegisterView extends StatelessWidget {
                     TextFormField(
                       decoration:
                           const InputDecoration(hintText: "Mobile number"),
-                      controller: phoneController,
+                      controller: controller.phoneController,
                       validator: (value) =>
                           AppValidator.validatePhoneNumber(value),
                     ),
@@ -73,20 +85,20 @@ class RegisterView extends StatelessWidget {
                     ),
                     Obx(() =>
                       TextFormField(
-                        obscureText: !passwordVisible.value,
+                        obscureText: !controller.passwordVisible.value,
                         decoration: InputDecoration(
                             hintText: "Password",
                             suffixIcon: IconButton(
                               icon: Icon(
-                                passwordVisible.value
+                                controller.passwordVisible.value
                                     ? CupertinoIcons.eye_fill
                                     : CupertinoIcons.eye_slash,
                               ),
                               onPressed: () {
-                                passwordVisible.value = !passwordVisible.value;
+                                controller.passwordVisible.value = !controller.passwordVisible.value;
                               },
                             )),
-                        controller: passwordController,
+                        controller: controller.passwordController,
                         validator: (value) =>
                             AppValidator.validatePassword(value),
                       ),
@@ -96,26 +108,27 @@ class RegisterView extends StatelessWidget {
                     ),
                     Obx(() =>
                       TextFormField(
-                        obscureText: !passwordVisible.value,
+                        obscureText: !controller.passwordVisible.value,
                         decoration: InputDecoration(
                             hintText: "Confirm password",
                             suffixIcon: IconButton(
                               icon: Icon(
-                                passwordVisible.value
+                                controller.passwordVisible.value
                                     ? CupertinoIcons.eye_fill
                                     : CupertinoIcons.eye_slash,
                               ),
                               onPressed: () {
-                                passwordVisible.value = !passwordVisible.value;
+                                controller.passwordVisible.value = !controller.passwordVisible.value;
                               },
                             )),
-                        controller: confirmPasswordController,
+                        controller: controller.confirmPasswordController,
                         validator: (value) =>
-                            AppValidator.validateConfirmPassword(passwordController.text, value),
+                            AppValidator.validateConfirmPassword(controller.passwordController.text, value),
                       ),
                     ),
+                    const SizedBox(height: Sizes.spaceBtwInputFields),
                     Row(children: [
-                      Checkbox(value: true, onChanged: (value) {}),
+                      SizedBox(height: 24, width: 24, child: Obx(() => Checkbox(value: controller.termsAndConditions.value, onChanged: (value) {controller.termsAndConditions.value = value!;}))),
                       Text("termsAndConditions".tr)
                     ]),
                     const SizedBox(height: Sizes.spaceBtwSections),
@@ -125,9 +138,7 @@ class RegisterView extends StatelessWidget {
                   children: [
                     FilledButton(
                         onPressed: () {
-                          if (formKey.currentState?.validate() ?? false) {
-                            Get.snackbar("Success", "Validated");
-                          }
+                          controller.register();
                         },
                         style: ButtonStyle(
                           backgroundColor: WidgetStateProperty.all(
