8d9ff44f8e9ca61276f6fc23b6b429d2ac07ebfe
minhazul73 <minhazul73@gmail.com>
2025-04-24 01:30:27 +0600

    Refactor button theme styles for improved readability and consistency

--- lib/utils/theme/widget_themes/filled_button_theme.dart
+++ lib/utils/theme/widget_themes/filled_button_theme.dart
@@ -6,18 +6,18 @@ import '../../constants/sizes.dart';
 class AppFilledButtonTheme {
   
   static final lightfilledButtonTheme = FilledButtonThemeData(
-      style: FilledButton.styleFrom(
-    elevation: 0,
-    backgroundColor: AppColor.primary,
-    // foregroundColor: AppColor.light,
-    disabledBackgroundColor: AppColor.darkGrey,
-    // disabledForegroundColor: AppColor.darkGrey,
-    side: const BorderSide(color: AppColor.primary),
-    minimumSize: const Size(Sizes.buttonWidth, Sizes.buttonHeight),
-    textStyle: const TextStyle(
-        fontSize: 16, color: AppColor.textWhite, fontWeight: FontWeight.w600),
-    shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(Sizes.buttonRadius)),
+    style: FilledButton.styleFrom(
+      elevation: 0,
+      backgroundColor: AppColor.primary,
+      // foregroundColor: AppColor.light,
+      disabledBackgroundColor: AppColor.darkGrey,
+      // disabledForegroundColor: AppColor.darkGrey,
+      side: const BorderSide(color: AppColor.primary),
+      minimumSize: const Size(Sizes.buttonWidth, Sizes.buttonHeight),
+      textStyle: const TextStyle(
+          fontSize: 16, color: AppColor.textWhite, fontWeight: FontWeight.w600),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(Sizes.buttonRadius)),
   ));
 
   static final darkfilledButtonTheme = FilledButtonThemeData(
