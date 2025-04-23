89a85aea75db94a1242fcd83a249e0cd810a5f29
minhazul73 <minhazul73@gmail.com>
2025-04-24 01:27:35 +0600

    Add xxl size constant to Sizes for improved layout flexibility

--- lib/utils/constants/sizes.dart
+++ lib/utils/constants/sizes.dart
@@ -6,6 +6,7 @@ class Sizes {
   static const double md = 16.0;
   static const double lg = 24.0;
   static const double xl = 32.0;
+  static const double xxl = 48.0;
 
   // Icon sizes
   static const double iconXs = 12.0;
