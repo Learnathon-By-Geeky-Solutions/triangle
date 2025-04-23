31096ae1e4c01011c1267c2ff38f79c0d03ad344
minhazul73 <minhazul73@gmail.com>
2025-04-24 02:07:40 +0600

    Enhance HomeView layout with updated date display and new activity counters

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -16,13 +16,13 @@ class HomeView extends StatelessWidget {
             Text("Baby Shophia",
               style: Theme.of(context).textTheme.titleMedium,),
             Text("Born in October 25, 2024",
-              style: Theme.of(context).textTheme.bodySmall,),
+              style: Theme.of(context).textTheme.labelMedium,),
           ],
         ),
         backgroundColor: AppColor.softGrey,
         centerTitle: true,
         leading: const Icon(Icons.menu),
-        actionsPadding: const EdgeInsets.only(right: Sizes.sm),
+        actionsPadding: const EdgeInsets.only(right: Sizes.md),
         actions: const [
           CircleAvatar(
             radius: Sizes.md,
@@ -34,19 +34,167 @@ class HomeView extends StatelessWidget {
         padding: const EdgeInsets.symmetric(
             horizontal: Sizes.md, vertical: Sizes.sm),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(AppHelperFunctions.getTodaysDate()),
+                    Text(AppHelperFunctions.getTodaysDate(),
+                      style: Theme.of(context).textTheme.labelMedium,),
                     Text("Today",
                       style: Theme.of(context).textTheme.bodyLarge,),
-                  ],
-                )
+                  ]
+                ),
+                FilledButton(onPressed: () {},
+                  style: FilledButton.styleFrom(
+                    minimumSize: const Size(Sizes.xl, Sizes.xl),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(Sizes.sm)),
+                    backgroundColor: AppColor.secondary,
+                    side: const BorderSide(color: AppColor.secondary),
+                  ), 
+                  child: Text("Update",
+                    style: Theme.of(context).textTheme.bodyLarge!.apply(color: AppColor.light),
+                  ),
+                ),
               ],
-            )
+            ),
+
+            const SizedBox(height: Sizes.spaceBtwItems),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: Sizes.md),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(Sizes.md),
+                    color: AppColor.mayaBlue,
+                  ),
+                  constraints: const BoxConstraints(
+                    minWidth: 80,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Years",
+                        style: Theme.of(context).textTheme.headlineSmall!.apply(color: AppColor.light),),
+                      const SizedBox(height: Sizes.xs),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(Sizes.sm),
+                          color: AppColor.light,
+                        ),
+                        constraints: const BoxConstraints(
+                          maxHeight: Sizes.xxl,
+                          maxWidth: Sizes.xxl,
+                        ),
+                        child: Center(child: Text("0", style: Theme.of(context).textTheme.headlineMedium,))
+                      )
+                    ],
+                  ),
+                ),
+                              
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: Sizes.md),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(Sizes.md),
+                    color: AppColor.hotPink,
+                  ),
+                  constraints: const BoxConstraints(
+                    minWidth: 80,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Months",
+                        style: Theme.of(context).textTheme.headlineSmall!.apply(color: AppColor.light),),
+                      const SizedBox(height: Sizes.xs),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(Sizes.sm),
+                          color: AppColor.light,
+                        ),
+                        constraints: const BoxConstraints(
+                          maxHeight: Sizes.xxl,
+                          maxWidth: Sizes.xxl,
+                        ),
+                        child: Center(child: Text("0", style: Theme.of(context).textTheme.headlineMedium,))
+                      )
+                    ],
+                  ),
+                ),
+                
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: Sizes.md),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(Sizes.md),
+                    color: AppColor.brownYellow,
+                  ),
+                  constraints: const BoxConstraints(
+                    minWidth: 80,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Weeks",
+                        style: Theme.of(context).textTheme.headlineSmall!.apply(color: AppColor.light),),
+                      const SizedBox(height: Sizes.xs),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(Sizes.sm),
+                          color: AppColor.light,
+                        ),
+                        constraints: const BoxConstraints(
+                          maxHeight: Sizes.xxl,
+                          maxWidth: Sizes.xxl,
+                        ),
+                        child: Center(child: Text("0", style: Theme.of(context).textTheme.headlineMedium,))
+                      )
+                    ],
+                  ),
+                ),
+                
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: Sizes.md),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(Sizes.md),
+                    color: AppColor.pinkFlamingo,
+                  ),
+                  constraints: const BoxConstraints(
+                    minWidth: 80,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Days",
+                        style: Theme.of(context).textTheme.headlineSmall!.apply(color: AppColor.light),),
+                      const SizedBox(height: Sizes.xs),
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(Sizes.sm),
+                          color: AppColor.light,
+                        ),
+                        constraints: const BoxConstraints(
+                          maxHeight: Sizes.xxl,
+                          maxWidth: Sizes.xxl,
+                        ),
+                        child: Center(child: Text("0", style: Theme.of(context).textTheme.headlineMedium,))
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            
+            const SizedBox(height: Sizes.spaceBtwItems),
+
+            Text("Activities",
+              style: Theme.of(context).textTheme.headlineMedium),
+
+            
           ],
         ),
       ),
