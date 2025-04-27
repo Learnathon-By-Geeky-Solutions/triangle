6694cf1c3f605012ff90a5a123fb2faf9764d448
minhazul73 <minhazul73@gmail.com>
2025-04-27 22:27:45 +0600

    Add AppbarTheme class to define light and dark AppBar themes for consistent UI

---
+++ lib/utils/theme/widget_themes/app_bar_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:triangle/utils/constants/app_colors.dart';
+import 'package:triangle/utils/constants/sizes%20copy.dart';
+
+class AppbarTheme {
+  static AppBarTheme lightAppBarTheme = const AppBarTheme(
+    elevation: 0,
+    centerTitle: true,
+    foregroundColor: AppColor.black,
+    backgroundColor: AppColor.softGrey,
+    actionsPadding: EdgeInsets.only(right: Sizes.md),
+    iconTheme: IconThemeData(color: AppColor.black),
+    actionsIconTheme: IconThemeData(color: AppColor.black),
+  );
+
+  static AppBarTheme darkAppBarTheme = const AppBarTheme(
+    elevation: 0,
+    centerTitle: true,
+    foregroundColor: AppColor.white,
+    backgroundColor: AppColor.darkerGrey,
+    actionsPadding: EdgeInsets.only(right: Sizes.md),
+    iconTheme: IconThemeData(color: AppColor.white),
+    actionsIconTheme: IconThemeData(color: AppColor.white),
+  );
+}
