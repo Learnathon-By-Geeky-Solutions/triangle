f3cb760617fdfa8300296c8ff49a0acedc841b0f
minhazul73 <minhazul73@gmail.com>
2025-04-28 21:15:58 +0600

    Remove unused imports and simplify Firebase initialization in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:triangle/data/repositories/auth/authentication_repository.dart';
 import 'app.dart';
 import 'firebase_options.dart';
 
@@ -13,9 +11,7 @@ void main() async {
   await GetStorage.init();
   await dotenv.load(fileName: ".env");
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then(
-    (FirebaseApp _) => Get.put(AuthenticationRepository()),
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const LittleSteps());
 }
 
