a00c3128c71fde92853a78d199bdd0b723a51c33
minhazul73 <minhazul73@gmail.com>
2025-04-28 21:15:49 +0600

    Initialize Sizes class with current context in LittleSteps widget

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:triangle/utils/constants/sizes.dart';
 import 'bindings/general_bindings.dart';
 import 'utils/theme/theme.dart';
 import 'utils/translations/localization_service.dart';
@@ -9,6 +10,10 @@ class LittleSteps extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    // Initialize the Sizes class with the current context
+    Sizes.init(context);
+    
     return GetMaterialApp(
       title: 'Little Steps',
       debugShowCheckedModeBanner: false,
