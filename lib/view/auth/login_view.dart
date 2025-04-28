ae5c055e9c1bb9600c0aae111c08bb822c2e1fef
minhazul73 <minhazul73@gmail.com>
2025-04-28 21:14:48 +0600

    Refactor LoginView to use Get.find for LoginController, improving dependency management

--- lib/view/auth/login_view.dart
+++ lib/view/auth/login_view.dart
@@ -14,7 +14,7 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final controller = Get.put(LoginController());
+    final controller = Get.find<LoginController>();
 
     return Scaffold(
         body: Padding(
