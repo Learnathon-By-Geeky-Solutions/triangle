69fd703e9fcead33945232d3c422845410ac825f
minhazul73 <minhazul73@gmail.com>
2025-04-28 21:13:06 +0600

    Refactor GeneralBindings to improve dependency management and initialization order for controllers and repositories

--- lib/bindings/general_bindings.dart
+++ lib/bindings/general_bindings.dart
@@ -1,9 +1,26 @@
 import 'package:get/get.dart';
+import 'package:triangle/data/repositories/auth/authentication_repository.dart';
+import 'package:triangle/data/repositories/auth/authentication_repository_impl.dart';
+import 'package:triangle/data/repositories/user/user_repository.dart';
 import 'package:triangle/utils/helpers/network_manager.dart';
+import 'package:triangle/view_model/auth/initial_screen_redirect.dart';
+import 'package:triangle/view_model/auth/login_controller.dart';
+import 'package:triangle/view_model/auth/register_controller.dart';
+
+import '../data/repositories/user/user_repository_impl.dart';
 
 class GeneralBindings extends Bindings {
   @override
   void dependencies() {
-    Get.put(NetworkManager());
+    Get.put(InitialScreenRedirect());
+    Get.lazyPut(() => NetworkManager());
+
+    // Repository bindings
+    Get.lazyPut<UserRepository>(() => UserRepositoryImpl());
+    Get.put<AuthenticationRepository>(AuthenticationRepositoryImpl());
+
+    // Controller bindings
+    Get.lazyPut<RegisterController>(() => RegisterController(repository: Get.find<UserRepository>()));
+    Get.lazyPut<LoginController>(() => LoginController(repository: Get.find<AuthenticationRepository>()));
   }
 }
