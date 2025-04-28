d9f6a19e268809edeff14edb351b2d6fbfbbc120
minhazul73 <minhazul73@gmail.com>
2025-04-28 20:55:36 +0600

    Implement UserRepositoryImpl to handle user data storage with Firestore

---
+++ lib/data/repositories/user/user_repository_impl.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
+import 'package:triangle/data/models/user/user_model.dart';
+import 'package:triangle/data/repositories/user/user_repository.dart';
+import 'package:triangle/utils/exceptions/app_exceptions.dart';
+import 'package:triangle/utils/exceptions/format_exceptions.dart';
+
+class UserRepositoryImpl extends UserRepository {
+
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  @override
+  Future<void> saveUserData(UserModel user) async {
+    try {
+      await _firestore.collection("users").doc(user.id).set(user.toJson());
+    } on FirebaseException catch (e) {
+      throw AppFirebaseException(e.code);
+    } on FormatException catch (_) {
+      throw const AppFormatException();
+    } on PlatformException catch (e) {
+      throw AppPlatformException(e.code);
+    } catch (e) {
+      throw "An unexpected error occurred. Please try again.";
+    }
+  }
+}
