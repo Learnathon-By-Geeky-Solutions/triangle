caac5374c933d16952800b94de0de1a20d45e3f9
minhazul73 <minhazul73@gmail.com>
2025-04-30 01:57:39 +0600

    Add helper functions for displaying snack bars and alerts, and truncating text

--- lib/utils/helpers/helper_functions.dart
+++ lib/utils/helpers/helper_functions.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 
 class AppHelperFunctions {
@@ -5,4 +7,37 @@ class AppHelperFunctions {
     final DateTime now = DateTime.now();
     return DateFormat('EEEE, MMMM d').format(now);
   }
+
+  static void showSnackBar(String message) {
+    ScaffoldMessenger.of(Get.context!).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
+  }
+
+  static void showAlert(String title, String message) {
+    showDialog(
+      context: Get.context!,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  static String truncateText(String text, int maxLength) {
+    if (text.length <= maxLength) {
+      return text;
+    } else {
+      return '${text.substring(0, maxLength)}...';
+    }
+  }
+
 }
