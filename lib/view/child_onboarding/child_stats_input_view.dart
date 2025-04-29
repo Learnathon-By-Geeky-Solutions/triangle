5e6e622a7cfeb56f958b65b8deaa12b01fefae25
minhazul73 <minhazul73@gmail.com>
2025-04-29 14:18:23 +0600

    Refactor ChildStatsInputView to enhance form structure and input validation

--- lib/view/child_onboarding/child_stats_input_view.dart
+++ lib/view/child_onboarding/child_stats_input_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:triangle/view/home/home_view.dart';
+import 'package:triangle/utils/validators/app_validator.dart';
 import '../../utils/constants/sizes.dart';
 import '../../view_model/child_onboarding/child_onboarding_controller.dart';
 
@@ -17,43 +16,48 @@ class ChildStatsInputView extends StatelessWidget {
         height: MediaQuery.of(context).size.height,
         child: Padding(
           padding: const EdgeInsets.all(Sizes.lg),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Enter Child\'s Height & Weight',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              const SizedBox(height: Sizes.spaceBtwSections),
-              // Height Input
-              TextFormField(
-                controller: controller.heightController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  hintText: 'Enter height in centimeters',
-                  suffixText: 'cm',
+          child: Form(
+            key: controller.statsFormKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Enter Child\'s Height & Weight',
+                  style: Theme.of(context).textTheme.headlineMedium,
                 ),
-              ),
-              const SizedBox(height: Sizes.spaceBtwItems),
-              // Weight Input
-              TextFormField(
-                controller: controller.weightController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  hintText: 'Enter weight in kilograms',
-                  suffixText: 'kg',
+                const SizedBox(height: Sizes.spaceBtwSections),
+                // Height Input
+                TextFormField(
+                  controller: controller.heightController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter height in centimeters',
+                    suffixText: 'cm',
+                  ),
+                  validator: (value) => AppValidator.validatePositiveNumber(value),
                 ),
-              ),
-              const SizedBox(height: Sizes.spaceBtwSections),
-
-              // Continue Button
-              OutlinedButton(
-                onPressed: () {
-                  Get.to(() => const HomeView());
-                },
-                child: const Text("Continue"),
-              ),
-            ],
+                const SizedBox(height: Sizes.spaceBtwItems),
+                // Weight Input
+                TextFormField(
+                  controller: controller.weightController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter weight in kilograms',
+                    suffixText: 'kg',
+                  ),
+                  validator: (value) => AppValidator.validatePositiveNumber(value),
+                ),
+                const SizedBox(height: Sizes.spaceBtwSections),
+            
+                // Continue Button
+                OutlinedButton(
+                  onPressed: () {
+                    controller.saveChildData();                    
+                  },
+                  child: const Text("Continue"),
+                ),
+              ],
+            ),
           ),
         ),
       ),
