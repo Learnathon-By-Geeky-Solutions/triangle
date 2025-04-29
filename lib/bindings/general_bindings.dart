fc66383ee30b2eca3fe8163c6dc56fbf4119e3a2
minhazul73 <minhazul73@gmail.com>
2025-04-30 01:59:15 +0600

    Refactor GeneralBindings to improve repository and controller bindings for better dependency management

--- lib/bindings/general_bindings.dart
+++ lib/bindings/general_bindings.dart
@@ -8,7 +8,9 @@ import 'package:triangle/utils/helpers/network_manager.dart';
 import 'package:triangle/view_model/auth/initial_screen_redirect.dart';
 import 'package:triangle/view_model/auth/login_controller.dart';
 import 'package:triangle/view_model/auth/register_controller.dart';
+import 'package:triangle/view_model/child/child_controller.dart';
 import 'package:triangle/view_model/child_onboarding/child_onboarding_controller.dart';
+import 'package:triangle/view_model/home/home_controller.dart';
 
 import '../data/repositories/user/user_repository_impl.dart';
 
@@ -19,13 +21,15 @@ class GeneralBindings extends Bindings {
     Get.lazyPut(() => NetworkManager());
 
     // Repository bindings
-    Get.lazyPut<UserRepository>(() => UserRepositoryImpl());
-    Get.put<AuthenticationRepository>(AuthenticationRepositoryImpl());
-    Get.lazyPut<ChildRepository>(() => ChildRepositoryImpl());
+    Get.lazyPut<AuthenticationRepository>(() => AuthenticationRepositoryImpl());
+    Get.lazyPut<UserRepository>(() => UserRepositoryImpl(authRepository: Get.find<AuthenticationRepository>()));
+    Get.lazyPut<ChildRepository>(() => ChildRepositoryImpl(userRepository: Get.find<UserRepository>()));
 
     // Controller bindings
     Get.lazyPut<RegisterController>(() => RegisterController(authRepository: Get.find<AuthenticationRepository>(), userRepository: Get.find<UserRepository>()));
     Get.lazyPut<LoginController>(() => LoginController(repository: Get.find<AuthenticationRepository>()));
     Get.lazyPut<ChildOnboardingController>(() => ChildOnboardingController(repository: Get.find<ChildRepository>()));
+    Get.lazyPut<ChildController>(() => ChildController(repository: Get.find<ChildRepository>()));
+    Get.lazyPut<HomeController>(() => HomeController(childController: Get.find<ChildController>()));
   }
 }
