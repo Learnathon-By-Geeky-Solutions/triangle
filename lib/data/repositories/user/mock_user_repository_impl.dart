38d536a49e1a34330f7f4c03e8509ce68a754c15
minhazul73 <minhazul73@gmail.com>
2025-04-29 14:23:58 +0600

    Refactor saveUserData method in MockUserRepositoryImpl to return a String instead of void

--- lib/data/repositories/user/mock_user_repository_impl.dart
+++ lib/data/repositories/user/mock_user_repository_impl.dart
@@ -4,9 +4,9 @@ import '../../models/user/user_model.dart';
 
 class MockUserRepositoryImpl extends UserRepository {
   @override
-  Future<void> saveUserData(UserModel user) async {
+  Future<String> saveUserData(UserModel user) async {
     // Mock implementation: Simulate a delay and return without doing anything
     await Future.delayed(const Duration(seconds: 1));
-    return;
+    return "";
   }
 }
