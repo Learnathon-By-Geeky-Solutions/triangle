42b7d9b37c18ceeea428b47216aea7b586c8fa0b
minhazul73 <minhazul73@gmail.com>
2025-04-30 01:56:50 +0600

    Refactor AuthenticationRepositoryImpl by removing unused methods and imports for improved clarity

--- lib/data/repositories/auth/authentication_repository_impl.dart
+++ lib/data/repositories/auth/authentication_repository_impl.dart
@@ -1,44 +1,25 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:triangle/data/repositories/auth/authentication_repository.dart';
 import 'package:triangle/utils/exceptions/format_exceptions.dart';
-import 'package:triangle/view/child_onboarding/child_name_input_view.dart';
 
 import '../../../utils/exceptions/app_exceptions.dart';
 import '../../../view/auth/login_view.dart';
-import '../../../view/auth/register_view.dart';
 
 class AuthenticationRepositoryImpl extends AuthenticationRepository {
-  static AuthenticationRepositoryImpl get instance => Get.find();
+  final FirebaseAuth _auth;
 
-  final deviceStorage = GetStorage();
-  final _auth = FirebaseAuth.instance;
-  static const commonErrorMessage = "An unexpected error occurred. Please try again.";
+  AuthenticationRepositoryImpl({FirebaseAuth? auth})
+      : _auth = auth ?? FirebaseAuth.instance;
   
-  @override
-  Future<void> onReady() async {
-    FlutterNativeSplash.remove();
-    await screenRedirect();
-  }
+  final deviceStorage = GetStorage();
+  static const commonErrorMessage = "An unexpected error occurred. Please try again.";
 
   @override
-  Future<void> screenRedirect() async {
-    final user = _auth.currentUser;
-    if (user != null) {
-      Get.offAll(() => const ChildNameInputView());
-    }
-    else {
-      deviceStorage.writeIfNull('isFirstTime', true);
-      deviceStorage.read('isFirstTime') != true
-          ? Get.offAll(() => const RegisterView())
-          : Get.offAll(() => const LoginView());
-    }
-  }
-
-
+  User? get currentUser => _auth.currentUser;
+  
   @override
   Future<UserCredential> loginWithEmailAndPassword(String email, String password) async {
     try {
