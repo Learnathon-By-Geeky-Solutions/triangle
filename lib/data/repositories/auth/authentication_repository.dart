2465cc939f06cab28073afcdaa2766bf5f174443
minhazul73 <minhazul73@gmail.com>
2025-04-30 01:53:55 +0600

    Remove unused methods from AuthenticationRepository for cleaner interface

--- lib/data/repositories/auth/authentication_repository.dart
+++ lib/data/repositories/auth/authentication_repository.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 
 abstract class AuthenticationRepository {
-  Future<void> onReady();
-  Future<void> screenRedirect();
+  User? get currentUser;
   Future<UserCredential> loginWithEmailAndPassword(String email, String password);
   Future<UserCredential> registerWithEmailAndPassword(String email, String password);
   Future<void> logout();
