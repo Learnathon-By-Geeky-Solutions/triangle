0be281ea2614315c790c3218e7e1374a28db4da5
minhazul73 <minhazul73@gmail.com>
2025-04-30 01:59:38 +0600

    Refactor AgeGridLayout to utilize HomeController for dynamic age data rendering

--- lib/view/home/widgets/age_grid_layout.dart
+++ lib/view/home/widgets/age_grid_layout.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:triangle/view_model/home/home_controller.dart';
 
 import '../../../utils/constants/app_colors.dart';
 import '../../../utils/constants/sizes.dart';
 
 class AgeGridLayout extends StatelessWidget {
-  AgeGridLayout({super.key});
+  const AgeGridLayout({super.key});
 
-  final List ageGrid = [
-    ["Years", AppColor.mayaBlue, "0"],
-    ["Months", AppColor.hotPink, "0"],
-    ["Weeks", AppColor.brownYellow, "0"],
-    ["Days", AppColor.pinkFlamingo, "0"],
-  ];
 
   @override
   Widget build(BuildContext context) {
+
+    final controller = Get.find<HomeController>();
+    
     return Scaffold(
       body: GridView.builder(
         itemCount: 4,
@@ -25,33 +24,34 @@ class AgeGridLayout extends StatelessWidget {
           childAspectRatio: .72,
         ), 
         itemBuilder: (BuildContext context, int index) {
-          return Container(
-            padding: EdgeInsets.symmetric(vertical: Sizes.rmd),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(Sizes.md),
-              color: ageGrid[index][1],
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(ageGrid[index][0],
-                  style: Theme.of(context).textTheme.headlineSmall!.apply(color: AppColor.light),),
-                SizedBox(height: Sizes.rxs),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(Sizes.sm),
-                    color: AppColor.light,
-                  ),
-                  constraints: BoxConstraints(
-                    maxHeight: Sizes.rxxl,
-                    maxWidth: Sizes.rxxl,
-                  ),
-                  child: Center(child: Text(ageGrid[index][2], style: Theme.of(context).textTheme.headlineMedium!.apply(color: AppColor.black),))
-                )
-              ],
+          return Obx(() =>
+            Container(
+              padding: EdgeInsets.symmetric(vertical: Sizes.rmd),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Sizes.md),
+                color: controller.ageGrid[index][1],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(controller.ageGrid[index][0],
+                    style: Theme.of(context).textTheme.headlineSmall!.apply(color: AppColor.light),),
+                  SizedBox(height: Sizes.rxs),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(Sizes.sm),
+                      color: AppColor.light,
+                    ),
+                    constraints: BoxConstraints(
+                      maxHeight: Sizes.rxxl,
+                      maxWidth: Sizes.rxxl,
+                    ),
+                    child: Center(child: Text(controller.ageGrid[index][2], style: Theme.of(context).textTheme.headlineMedium!.apply(color: AppColor.black),))
+                  )
+                ],
+              ),
             ),
           );
-              
         },
       ),
     );
