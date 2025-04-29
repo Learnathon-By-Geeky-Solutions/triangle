2416292bb6a73644d82ab9cbd7ba7c48b0d4b2c7
minhazul73 <minhazul73@gmail.com>
2025-04-30 01:57:03 +0600

    Add userId getter and getUserData method to UserRepository for enhanced functionality

--- lib/data/repositories/user/user_repository.dart
+++ lib/data/repositories/user/user_repository.dart
@@ -1,5 +1,7 @@
 import '../../models/user/user_model.dart';
 
 abstract class UserRepository {
+  String? get userId;
   Future<String> saveUserData(UserModel user);
+  Future<UserModel?> getUserData();
 }
