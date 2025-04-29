76f16d3a3f385fdbfe4c5f7bf3d7a668c7b2bd7f
minhazul73 <minhazul73@gmail.com>
2025-04-30 01:58:31 +0600

    Refactor ChildRepositoryImpl to integrate UserRepository for enhanced child data retrieval and updating

--- lib/data/repositories/child/child_repository_impl.dart
+++ lib/data/repositories/child/child_repository_impl.dart
@@ -1,15 +1,21 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
 import 'package:triangle/data/models/child/child_model.dart';
 import 'package:triangle/data/repositories/child/child_repository.dart';
+import 'package:triangle/data/repositories/user/user_repository.dart';
 import 'package:triangle/utils/exceptions/app_exceptions.dart';
 import 'package:triangle/utils/exceptions/format_exceptions.dart';
 
 class ChildRepositoryImpl implements ChildRepository {
+  final UserRepository _userRepository;
   final FirebaseFirestore _firestore;
 
-  ChildRepositoryImpl({FirebaseFirestore? firestore})
-      : _firestore = firestore ?? FirebaseFirestore.instance;
+  ChildRepositoryImpl({
+    required UserRepository userRepository,
+    FirebaseFirestore? firestore,
+  }) : _userRepository = userRepository, 
+       _firestore = firestore ?? FirebaseFirestore.instance;
 
   @override
   Future<String> saveChild(ChildModel child) async {
@@ -32,16 +38,82 @@ class ChildRepositoryImpl implements ChildRepository {
     }
   }
 
+  
   @override
-  Future<List<ChildModel>> getChildren(String userId) async {
-    final querySnapshot = await _firestore
-        .collection('users')
-        .doc(userId)
-        .collection('children')
-        .get();
+  Future<String> getChildId() async {
+    try {
+      final querySnapshot = await _firestore
+          .collection('users')
+          .doc(_userRepository.userId)
+          .collection('children')
+          .limit(1)
+          .get();
+      
+      if (querySnapshot.docs.isNotEmpty) {
+        return querySnapshot.docs.first.id;
+      } else {
+        return ChildModel.empty().id!;
+      }
+    } on FirebaseAuthException catch (e) {
+      throw AppFirebaseAuthException(e.code);
+    } on FirebaseException catch (e) {
+      throw AppFirebaseException(e.code);
+    } on FormatException catch (_) {
+      throw const AppFormatException();
+    } on PlatformException catch (e) {
+      throw AppPlatformException(e.code);
+    } catch (e) {
+      throw "An unexpected error occurred. Please try again.";
+    }
+  }
 
-    return querySnapshot.docs
-        .map((doc) => ChildModel.fromSnapshot(doc))
-        .toList();
+  @override
+  Future<ChildModel> getChildData() async {
+    
+    try {
+      final querySnapshot = await _firestore
+          .collection('users')
+          .doc(_userRepository.userId)
+          .collection('children')
+          .limit(1)
+          .get();
+
+      if (querySnapshot.docs.isNotEmpty) {
+        return ChildModel.fromSnapshot(querySnapshot.docs.first);
+      } else {
+        return ChildModel.empty();
+      }
+    } on FirebaseAuthException catch (e) {
+      throw AppFirebaseAuthException(e.code);
+    } on FirebaseException catch (e) {
+      throw AppFirebaseException(e.code);
+    } on FormatException catch (_) {
+      throw const AppFormatException();
+    } on PlatformException catch (e) {
+      throw AppPlatformException(e.code);
+    } catch (e) {
+      throw "An unexpected error occurred. Please try again.";
+    }
+  }
+
+  @override
+  Future<void> updateChildData(ChildModel child) async {
+    try {
+      final docRef = _firestore
+          .collection("users")
+          .doc(child.parentId)
+          .collection("children")
+          .doc(child.id);
+
+      await docRef.update(child.toJson());
+    } on FirebaseException catch (e) {
+      throw AppFirebaseException(e.code);
+    } on FormatException catch (_) {
+      throw const AppFormatException();
+    } on PlatformException catch (e) {
+      throw AppPlatformException(e.code);
+    } catch (e) {
+      throw "An unexpected error occurred. Please try again.";
+    }
   }
 }
