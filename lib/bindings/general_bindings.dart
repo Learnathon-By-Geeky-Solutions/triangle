995e0dc148637c7979d6cff53ec946855eee8ee2
minhazul73 <minhazul73@gmail.com>
2025-04-29 14:22:02 +0600

    Add ChildRepository and ChildOnboardingController bindings in GeneralBindings

--- lib/bindings/general_bindings.dart
+++ lib/bindings/general_bindings.dart
@@ -1,11 +1,14 @@
 import 'package:get/get.dart';
 import 'package:triangle/data/repositories/auth/authentication_repository.dart';
 import 'package:triangle/data/repositories/auth/authentication_repository_impl.dart';
+import 'package:triangle/data/repositories/child/child_repository.dart';
+import 'package:triangle/data/repositories/child/child_repository_impl.dart';
 import 'package:triangle/data/repositories/user/user_repository.dart';
 import 'package:triangle/utils/helpers/network_manager.dart';
 import 'package:triangle/view_model/auth/initial_screen_redirect.dart';
 import 'package:triangle/view_model/auth/login_controller.dart';
 import 'package:triangle/view_model/auth/register_controller.dart';
+import 'package:triangle/view_model/child_onboarding/child_onboarding_controller.dart';
 
 import '../data/repositories/user/user_repository_impl.dart';
 
@@ -18,9 +21,11 @@ class GeneralBindings extends Bindings {
     // Repository bindings
     Get.lazyPut<UserRepository>(() => UserRepositoryImpl());
     Get.put<AuthenticationRepository>(AuthenticationRepositoryImpl());
+    Get.lazyPut<ChildRepository>(() => ChildRepositoryImpl());
 
     // Controller bindings
-    Get.lazyPut<RegisterController>(() => RegisterController(repository: Get.find<UserRepository>()));
+    Get.lazyPut<RegisterController>(() => RegisterController(authRepository: Get.find<AuthenticationRepository>(), userRepository: Get.find<UserRepository>()));
     Get.lazyPut<LoginController>(() => LoginController(repository: Get.find<AuthenticationRepository>()));
+    Get.lazyPut<ChildOnboardingController>(() => ChildOnboardingController(repository: Get.find<ChildRepository>()));
   }
 }
