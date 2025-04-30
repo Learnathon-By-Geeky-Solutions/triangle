ae1c96e8127f32f70c75bdfbce5112b620827b80
jannatulferdous2730 <jannatulferdous.2730@gmail.com>
2025-04-30 21:08:05 +0600

    Add AppAssets class for centralized asset management

---
+++ lib/utils/constants/asset_strings.dart
@@ -0,0 +1,21 @@
+class AppAssets {
+ 
+ // -- App Logo
+  static const String logo = "assets/images/app_logo.png";
+
+ // -- Splash Screen
+  static const String splashScreen = "assets/images/splash.png";
+
+ // -- Activities Grid Logo
+  static const String babySleeping = "assets/images/baby_sleep.png";
+  static const String milestone = "assets/images/milestone.png";
+  static const String babyFeeding = "assets/images/baby_feeding.png";
+  static const String vaccine = "assets/images/vaccine.png";
+
+ // -- Animation circular progress indicator
+  static const String loading = "assets/animations/animated_circular_loader.json";
+
+ // -- svg
+  static const String calendar = "assets/svg/calendar.svg";
+  static const String clockLine = "assets/svg/clarity_clock_line.svg";
+}
